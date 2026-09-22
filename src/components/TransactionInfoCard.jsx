d898ae81c49d43f6d72b757f4ae37cb127fac5ff
atharv96k <atharvmohite96k@gmail.com>
2026-09-22 15:00:01 +0530

    Fix delete button opacity in TransactionInfoCard component

--- src/components/TransactionInfoCard.jsx
+++ src/components/TransactionInfoCard.jsx
@@ -24,7 +24,7 @@ const TransactionInfoCard = ({icon, title, date, amount, type, hideDeleteBtn, on
                     {!hideDeleteBtn && (
                         <button
                             onClick={onDelete}
-                            className="text-gray-400 hover:text-red-800 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer">
+                            className="text-gray-400 hover:text-red-800 opacity-100 group-hover:opacity-100 transition-opacity cursor-pointer">
                             <Trash2 size={18} />
                         </button>
                     )}
